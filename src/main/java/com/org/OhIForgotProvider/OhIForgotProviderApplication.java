97ac9953c3cf96b2b2c396896fc41a857cd2f30c
Batshoun <andrew.batshoun@accenturefederal.com>
2022-11-30 14:20:45 -0600

    first commit

---
+++ src/main/java/com/org/OhIForgotProvider/OhIForgotProviderApplication.java
@@ -0,0 +1,13 @@
+package com.org.OhIForgotProvider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhIForgotProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhIForgotProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/OhIForgotProvider/OhIForgotProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.OhIForgotProvider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhIForgotProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
